7c7e91eee701fb10893d6d71ae6e01734310643c
Mikołaj <mikolajmikolaj090302@gmail.com>
2024-03-26 15:03:52 +0100

    Init zadanie 3

---
+++ Z03/LegacyApp.Tests/UserCreditServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.Annotations;
+using LegacyApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LegacyApp.Tests;
+
+[TestClass]
+[TestSubject(typeof(UserCreditService))]
+public class UserCreditServiceTest
+{
+
+    [TestMethod]
+    public void ShouldGetCreditServiceThrowException()
+    {
+        
+    }
+}
